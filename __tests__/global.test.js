c2e28cfae47c19271c0a8148a5f56c0a8f8b38cb
unknown <sergomorello@yandex.ru>
2023-06-02 16:50:02 +0300

    Исправил ошибки тестов

--- __tests__/global.test.js
+++ __tests__/global.test.js
@@ -1,13 +1,14 @@
 import Ext from "../lib";
 
 test('static', () => {
-    expect(typeof Ext.set).toBe('function');
+    expect(typeof Ext.setState).toBe('function');
 	expect(typeof Ext.useEvent).toBe('function');
 	expect(typeof Ext.useState).toBe('function');
 });
 
 test('dynamic', () => {
 	const ext = new Ext;
+	expect(typeof ext.setState).toBe('function');
 	expect(typeof ext.useEvent).toBe('function');
 	expect(typeof ext.useState).toBe('function');
 });
